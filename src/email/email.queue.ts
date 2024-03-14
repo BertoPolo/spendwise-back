536e46bcc2449354449a037bbfd7257b2853524b
Alberto Polo <bertopolo91@gmail.com>
2024-03-14 13:52:55 +0100

    .

--- src/email/email.queue.ts
+++
@@ -1,8 +0,0 @@
-import { Queue } from 'bullmq';
-
-export const emailQueue = new Queue('emailQueue', {
-  connection: {
-    host: 'localhost',
-    port: 6379,
-  },
-});
--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,9 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import * as nodemailer from 'nodemailer';
 import { SendEmailDto } from './email.interface';
-import { emailQueue } from './email.queue';
-// import { Processor, WorkerHost, OnWorkerEvent } from '@nestjs/bullmq';
-// import { Job } from 'bullmq';
 
 @Injectable()
 export class EmailService {
@@ -39,9 +36,5 @@ export class EmailService {
     } catch (error) {
       console.error(error);
     }
-  }
-
-  async queueEmailToSend(dto: SendEmailDto) {
-    await emailQueue.add('emailQueue', dto);
   }
 }
